31c9a5bdcd81f2c1ef77f46cd226d3efe620ecce
Alexandru Nicolaie <alexandru.nicolaie@htss.ro>
2024-08-04 16:12:35 +0300

    main.tf example

--- examples/base/main.tf
+++ examples/base/main.tf
@@ -1,4 +1,18 @@
-module "base_metadata_example" {
-    source = "../.."
-    // Here you'll be passing the input values into the module
+provider "azurerm" {
+  features {}
+}
+
+module "azurerm_service_plan" {
+  source                       = "../.."
+  service_plan_name            = "example-service-plan"
+  resource_group_name          = "example-resource-group"
+  location                     = "North Europe"
+  os_type                      = "Linux"
+  sku_name                     = "S1"
+  app_service_environment_id   = ""  
+  maximum_elastic_worker_count = ""
+  worker_count                 = ""
+  per_site_scaling_enabled     = false
+  zone_balancing_enabled       = false
+
 }
